df062e40df9e7a4dd3550ab9cea730dd978afae2
Thijs Dhulster <thijs.dhulster@gmail.com>
2024-05-28 08:40:49 +0200

    Initial langchain documentloader setup with SSL issues

---
+++ main.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import SitemapLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
+
+
+def main():
+    llm = ChatOpenAI()
+    loader = SitemapLoader("https://www.budgetyourtrip.com/sitemap.xml")
+
+    docs = loader.load()
+    print(docs)
+
+    embeddings = OpenAIEmbeddings()
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+    vector = FAISS.from_documents(documents, embeddings)
+
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+    <context>
+    {context}
+    </context>
+
+    Question: {input}""")
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+
+    # document_chain.invoke({
+    #     "input": "how can langsmith help with testing?",
+    #     "context": [Document(page_content="langsmith can let you visualize test results")]
+    # })
+
+    retriever = vector.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    response = retrieval_chain.invoke({"input": "How much would it cost to travel for 4 weeks with a budget travel style en Euro?"})
+    print(response["answer"])
+
+
+if __name__ == "__main__":
+    main()
