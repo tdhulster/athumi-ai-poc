61a1c9e7764025fe530d454a67eb8cb7b2de57a2
Thijs Dhulster <thijs.dhulster@gmail.com>
2024-05-29 16:26:49 +0200

    use gpt model 4o

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from dotenv import load_dotenv
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -12,11 +14,14 @@ load_dotenv()
 
 
 def main():
-    llm = ChatOpenAI()
-    loader = SitemapLoader("https://www.budgetyourtrip.com/sitemap.xml")
-    loader.session.headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+    llm = ChatOpenAI(model_name="gpt-4o")
+    docs = []
+    countries = get_countries()
+    for country in countries:
+        loader = WebBaseLoader(country)
+        loader.session.headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+        docs = docs + loader.load()
 
-    docs = loader.load()
     print(docs)
 
     embeddings = OpenAIEmbeddings()
@@ -43,9 +48,19 @@ def main():
     retriever = vector.as_retriever()
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-    response = retrieval_chain.invoke({"input": "How much would it cost to travel for 4 weeks with a budget travel style en Euro?"})
+    response = retrieval_chain.invoke({"input": "How much would it cost to travel for 4 weeks to albania with a budget travel style in Euro when we don't have to pay for sleep accomodation since we will travel by camper?"})
     print(response["answer"])
+
+def get_countries()->list[str]:
+    return [
+        "https://www.budgetyourtrip.com/albania",
+        "https://www.budgetyourtrip.com/croatia",
+        "https://www.budgetyourtrip.com/georgia",
+        "https://www.budgetyourtrip.com/bulgaria",
+        "https://www.budgetyourtrip.com/budgetreportadv.php?geonameid=&countrysearch=&country_code=AL&categoryid=0&budgettype=1&triptype=0&startdate=&enddate=&travelerno=0"
+    ]
 
 
 if __name__ == "__main__":
     main()
+
